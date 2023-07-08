8b09153520d2b5451a602e9f32877db5e0549ca6
JustDevWeb <zm91672@gmail.com>
2023-07-08 16:30:58 +0500

    change switch to if-else in profileDataParser , was some problem with maven

--- src/main/java/com/epam/mjc/io/ProfileDataParser.java
+++ src/main/java/com/epam/mjc/io/ProfileDataParser.java
@@ -1,12 +1,14 @@
 package com.epam.mjc.io;
 
-public class ProfileDataParser {
+import java.util.Objects;
 
+public class ProfileDataParser {
     private String data;
     private String name = null;
     private String email = null;
     private String phone = null;
     private String age = null;
+
     public ProfileDataParser(String data) {
         this.data = data;
         this.parseData(data);
@@ -20,11 +22,14 @@ public class ProfileDataParser {
             String key = values[0];
             String value = values[1].trim();
 
-            switch (key) {
-                case "Name" -> this.name = value;
-                case "Age" -> this.age = value;
-                case "Email" -> this.email = value;
-                case "Phone" -> this.phone = value;
+            if (Objects.equals(key, "Name")) {
+                name = value;
+            } else if (Objects.equals(key, "Age")) {
+                this.age = value;
+            } else if (Objects.equals(key, "Email")) {
+                this.email = value;
+            } else if (Objects.equals(key, "Phone")) {
+                this.phone = value;
             }
         }
     }
@@ -32,12 +37,15 @@ public class ProfileDataParser {
     public String getName() {
         return name;
     }
+
     public int getAge() {
         return Integer.parseInt(age);
     }
+
     public Long getPhone() {
         return Long.parseLong(phone);
     }
+
     public String getEmail() {
         return email;
     }
